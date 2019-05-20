2bf1ac35b2aa628e6aaaa50bdfbd36111cb1cb53
Alex Whan <alexwhan@gmail.com>
2019-05-20 15:35:14 +1000

    bug fixes, missing arguments

--- scripts/update_netcdf_cmd.R
+++ scripts/update_netcdf_cmd.R
@@ -1,4 +1,4 @@
 source('scripts/senaps.R')
 time <- Sys.time()
 attributes(time)$tzone <- "UTC"
-update_nc("data/temperature.nc", "temperature", time, deployments)
+update_nc("data/temperature.nc", "temperature", time, deployments, grid_points = 5000)
